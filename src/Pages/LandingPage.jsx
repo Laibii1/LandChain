cb58cced5c8a5017478de845321429b4e3fbc9cf
Laiba Riaz <106284661+Laibii1@users.noreply.github.com>
2025-08-06 08:43:43 +0500

    Update LandingPage.jsx

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { New_Navbar } from "../Components/New_Navbar";
-import  {HeroSection3D}  from "../Components/Landing page/Hero_section_3D";
-import { PropertyCards } from "../Components/Landing page/Property_tockenization_cards_section";
-import { UniqueNftSection } from "../Components/Landing page/Unique_Nft";
-import { FutureInsightSection } from "../Components/Landing page/FutureInsight_section";
-import { PricingSection } from "../Components/Landing page/Pricing";
-import { QuoteSection } from "../Components/Landing page/Quote";
-import { MapSection } from "../Components/Landing page/Map";
+import  {HeroSection3D}  from "../Components/Landingpage/Hero_section_3D";
+import { PropertyCards } from "../Components/Landingpage/Property_tockenization_cards_section";
+import { UniqueNftSection } from "../Components/Landingpage/Unique_Nft";
+import { FutureInsightSection } from "../Components/Landingpage/FutureInsight_section";
+import { PricingSection } from "../Components/Landingpage/Pricing";
+import { QuoteSection } from "../Components/Landingpage/Quote";
+import { MapSection } from "../Components/Landingpage/Map";
 import Footer from "../Components/Footer";
 const LandingPage = () => {
   const navigate = useNavigate();
